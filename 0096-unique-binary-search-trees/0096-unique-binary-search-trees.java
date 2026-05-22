520bdbf53dd0c909fa3f84c658c628c18a43d380
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-05-22 12:57:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.1%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,23 +1,15 @@
 class Solution {
     public int numTrees(int n) {
-        //Catalan DP Approach
         int[] dp = new int[n+1];
         dp[0] = 1; dp[1] = 1;
-        for(int nodes = 2; nodes <= n; nodes++){
-            for(int root = 1; root <= nodes; root++){
-                int left = dp[root-1];
-                int right = dp[nodes - root];
-                dp[nodes] += left * right;
+
+        for(int node = 2; node <= n; node++){
+            for(int r = 1; r<= node; r++){
+                int left = dp[r - 1];
+                int right = dp[node - r];
+                dp[node] += left * right;
             }
         }
         return dp[n];
     }
 }
-// INNER LOOP → “Which node becomes root?”
-// Why dp[0] = 1?
-// Empty subtree is considered ONE valid arrangement.Otherwise multiplication breaks.
-// Example, If root is 1:
-// left subtree = empty
-// right subtree = 2 nodes
-// 1×dp[2]
-// not zero.
