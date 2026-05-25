a4096dddf1e02e036167b598791f350054f8e655
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-05-25 18:09:54 +0530

    Time: 1 ms (99.49%), Space: 42.6 MB (32.4%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int n) {
+        while(n > 1){
+            if(n % 5 == 0) n = n /= 5;
+            else if(n % 3 == 0) n /= 3;
+            else if(n % 2 == 0) n /= 2;
+            else return false;
+        }
+
+        if( n == 1) return true;
+        else return false;
+    }
+}
