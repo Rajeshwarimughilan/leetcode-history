90cc030ae17e2c27f3f13cf49607a672ca429c76
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-01 11:48:12 +0530

    Time: 3 ms (55.73%), Space: 45 MB (75.36%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        compute_subset(nums, new ArrayList<>(), 0);
+        return res;
+    }
+
+    public void compute_subset(int[] nums, ArrayList<Integer> curr, int start){
+        
+
+        
+        res.add(new ArrayList<>(curr));
+
+        for(int i = start; i < nums.length; i++){
+
+            if(i > start && nums[i] == nums[i-1]) continue;
+            
+            curr.add(nums[i]);
+            compute_subset(nums, curr, i + 1);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
