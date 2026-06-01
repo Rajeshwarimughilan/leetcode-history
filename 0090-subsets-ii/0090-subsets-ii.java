2966182af0f043f2ac6024b29bc0d725d339838c
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-01 12:01:21 +0530

    Time: 2 ms (99.67%), Space: 45.2 MB (23.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -8,17 +8,12 @@ class Solution {
     }
 
     public void compute_subset(int[] nums, ArrayList<Integer> curr, int start){
-        
-
-        
         res.add(new ArrayList<>(curr));
 
         for(int i = start; i < nums.length; i++){
-
-            if(i > start && nums[i] == nums[i-1]) continue;
-            
+            if(i > start && nums[i] == nums[i - 1]) continue;
             curr.add(nums[i]);
-            compute_subset(nums, curr, i + 1);
+            compute_subset(nums, curr, i+1);
             curr.remove(curr.size() - 1);
         }
     }
