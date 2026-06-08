06681f53140d5cba0d596d4d0505ab7fb74d8b12
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-08 10:30:15 +0530

    Time: 1 ms (59.23%), Space: 42.2 MB (82.4%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -4,7 +4,8 @@ class Solution {
         for(int i = 0; i < 32; i++){
             ans <<= 1; //make space
             ans |= (n & 1); //add last digit 
-            n /= 2;
+            // n /= 2; this works nly for positive number
+            n >>>= 1; //will work for both positive nad negative
         }
         return ans;
     }
