8100276d23133955a1cb09b78fac22cde98b718b
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-20 07:53:41 +0530

    Time: 1 ms (89.17%), Space: 43.3 MB (46.83%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(((word.charAt(0) >= 65 && word.charAt(0) <= 90) && islower(word, 1)) ||
-        islower(word, 0) || 
-        isupper(word)
-        ) return true;
-        return false;
-    }
-    public static boolean islower(String s, int start){
-        for(int i = start; i < s.length(); i++){
-            if(s.charAt(i) < 95 || s.charAt(i) > 122) return false;
+        int count = 0;
+        for(int i = 0; i < word.length(); i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                count++;
+            }
         }
-        return true;
-    }
 
-    public static boolean isupper(String s){
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) < 65 || s.charAt(i) > 90) return false;
-        }
-        return true;
+        if(count == 0 ||
+        count == 1 && Character.isUpperCase(word.charAt(0)) ||
+        count == word.length()) return true;
+        return false;
     }
 }
