6db46b0c2eafa4bfbc95253da3088c4ec0735a36
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-21 08:30:24 +0530

    Time: 1 ms (23.05%), Space: 42.4 MB (22.56%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findComplement(int num) {
+        StringBuilder sb = new StringBuilder();
+        while(num > 0){
+            sb.append(num % 2);
+            num /= 2;
+        }
+        String bin = sb.reverse().toString();
+        int idx = bin.length() - 1;
+        int res = 0;
+        for(char b : bin.toCharArray()){
+            if(b == '0'){
+                res += 1 * Math.pow(2, idx);
+            }
+            else{
+                res += 0 * Math.pow(2, idx);
+            }
+            idx--;
+        }
+        return res;
+    }
+}
