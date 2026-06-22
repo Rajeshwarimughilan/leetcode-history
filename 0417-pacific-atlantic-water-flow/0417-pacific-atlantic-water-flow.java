acdf9dadc3791b1b213fe608b142a7d00014e3b2
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-06-22 05:55:31 +0530

    Time: 265 ms (6.67%), Space: 47.9 MB (10%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,12 +1,10 @@
 class Solution {
-    boolean pacific = false;
-    boolean atlantic = false;
-
+    boolean pacific;
+    boolean atlantic;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> res = new ArrayList<>();
         int rows = heights.length;
         int cols = heights[0].length;
-        
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 pacific = false;
@@ -14,7 +12,7 @@ class Solution {
                 boolean[][] visited = new boolean[rows][cols];
                 check(heights, i, j, visited);
                 if(pacific && atlantic){
-                    ArrayList<Integer> curr = new ArrayList<>();
+                    List<Integer> curr = new ArrayList<>();
                     curr.add(i);curr.add(j);
                     res.add(curr);
                 }
@@ -27,20 +25,21 @@ class Solution {
         if(visited[i][j]) return;
         visited[i][j] = true;
 
-        if(i == 0 || j == 0)pacific = true;
-        if(i == grid.length-1 || j == grid[0].length-1) atlantic = true;
+        if(i == 0 || j == 0) pacific = true;
+        if(i == grid.length - 1 || j == grid[0].length - 1) atlantic = true;
 
         if(pacific && atlantic){
-            visited[i][j] = false;
             return;
         }
 
-        if(i - 1 >= 0 && grid[i-1][j] <= grid[i][j]) check(grid, i-1, j, visited);
         if(i + 1 < grid.length && grid[i+1][j] <= grid[i][j]) check(grid, i+1, j, visited);
-        if(j - 1 >= 0 && grid[i][j-1] <= grid[i][j]) check(grid, i, j-1, visited);
+
         if(j + 1 < grid[0].length && grid[i][j+1] <= grid[i][j]) check(grid, i, j+1, visited);
 
-        visited[i][j] = false;
-        return;
+        if(i - 1 >= 0 && grid[i-1][j] <= grid[i][j]) check(grid, i-1, j, visited);
+
+        if(j - 1 >= 0 && grid[i][j-1] <= grid[i][j]) check(grid, i, j-1, visited);
+
+
     }
 }
