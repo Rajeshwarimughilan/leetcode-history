e04488958d733a76db83d0cb5ff6cf3d0da508f1
Rajeswari M <155134558+Rajeshwarimughilan@users.noreply.github.com>
2026-07-29 11:56:41 +0530

    Time: 1 ms (98.78%), Space: 47.8 MB (56.53%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -8,22 +8,24 @@ class Solution {
         }
 
         for(int i = 0; i < flowerbed.length; i++){
-            if(i == 0){
-                if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
-                    flowerbed[0] = 1;
-                    n--;
+            if(flowerbed[i] == 0){
+                if(i == 0){
+                    if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
+                        flowerbed[0] = 1;
+                        n--;
+                    }
                 }
-            }
-            else if(i == flowerbed.length - 1){
-                if(flowerbed[i] == 0 && flowerbed[i-1] == 0){
-                    flowerbed[i] = 1;
-                    n--;
+                else if(i == flowerbed.length - 1){
+                    if(flowerbed[i] == 0 && flowerbed[i-1] == 0){
+                        flowerbed[i] = 1;
+                        n--;
+                    }
                 }
-            }
-            else{
-                if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
-                    flowerbed[i] = 1;
-                    n--;
+                else{
+                    if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
+                        flowerbed[i] = 1;
+                        n--;
+                    }
                 }
             }
         }
